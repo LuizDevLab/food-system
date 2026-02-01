e62164d444770573b5a4bd0730a9e98835e3f256
Luiz Henrique da Silva Ramos <luizgthub@gmail.com>
2026-02-01 20:08:35 -0300

    feat: add product header component

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,18 +1,20 @@
 import { Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 interface ProductsProps {
   products: Product[];
 }
 
 const Products = ({ products }: ProductsProps) => {
+  const { slug } = useParams<{ slug: string }>();
   return (
     <div className="space-y-3 px-1.5 py-3">
       {products.map((product) => (
         <Link
           key={product.id}
-          href="/"
+          href={`/${slug}/menu/${product.id}`}
           className="flex items-center justify-between gap-10 py-3 border-b"
         >
           <div>
@@ -20,14 +22,21 @@ const Products = ({ products }: ProductsProps) => {
             <p className="line-clamp-2 text-sm text-muted-foreground">
               {product.description}
             </p>
-            <p className="pt-3 text-sm font-semibold">{new Intl.NumberFormat("pt-br", {
-              style: "currency",
-              currency: "BRL"
-            }).format(product.price)}</p>
+            <p className="pt-3 text-sm font-semibold">
+              {new Intl.NumberFormat("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              }).format(product.price)}
+            </p>
           </div>
 
           <div className="relative min-h-[82px] min-w-[120px]">
-            <Image src={product.imageUrl} alt={product.name} fill className="rounded-lg object-contain"/>
+            <Image
+              src={product.imageUrl}
+              alt={product.name}
+              fill
+              className="rounded-lg object-contain"
+            />
           </div>
         </Link>
       ))}
