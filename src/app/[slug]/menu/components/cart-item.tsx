18ef053b6f5c2fbf935fdef75111440d35e8b5ed
Luiz Henrique da Silva Ramos <luizgthub@gmail.com>
2026-02-03 22:59:11 -0300

    feat: add iniitial cart items component

---
+++ src/app/[slug]/menu/components/cart-item.tsx
@@ -0,0 +1,16 @@
+import { CartProduct } from "../contexts/cart"
+
+interface CartItemProps {
+  item: CartProduct
+}
+
+
+const CartItem = () => {
+  return (
+    <div className="">
+
+    </div>
+  )
+}
+
+export default CartItem
